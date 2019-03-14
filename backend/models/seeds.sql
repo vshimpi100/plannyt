d11806ea649bfde8f1567d49d543ff9bbdb47491
Reese Fairbanks <reese@reeses-mbp.attlocal.net>
2019-03-14 00:07:43 -0400

    Added connection.js to the config. Added image to events data

--- backend/models/seeds.sql
+++ backend/models/seeds.sql
@@ -1,29 +1,29 @@
 INSERT INTO events (image, link, title, details, eventType, creator, location, location_address, isPrivate, nicheEvent, niche, score, tentative, attending, start_point, end_point, createDate, price, comments) 
-VALUES (" ", " ", "Bowling", "2 games of bowling with drinks", "Sports", "Reese", "Midtown Bowl", " ", true, true, "Blowing League", 8, " ", " ", "7:00pm", "10:00pm", "March 19, 2019", "$20", "Bring a friend. Food and drinks will be available.");
+VALUES ("../client/src/assets/css/plannytblack.png", " ", "Bowling", "2 games of bowling with drinks", "Sports", "Reese", "Midtown Bowl", " ", true, true, "Blowing League", 8, " ", " ", "7:00pm", "10:00pm", "March 19, 2019", "$20", "Bring a friend. Food and drinks will be available.");
 
 INSERT INTO events (image, link, title, details, eventType, creator, location, location_address, isPrivate, nicheEvent, niche, score, tentative, attending, start_point, end_point, createDate, price, comments) 
-VALUES (" ", " ", "Bar Crawl", "Start at Buckhead Saloon and make our way down the Buckhead strip", "Nightlife", "Vivek", "Buckhead Bars", " ", false, false, " ", 8, " ", " ", "9:00pm", "1:00am", "March 29, 2019", " ", "Bring a friend. Party all night.");
+VALUES ("../client/src/assets/css/plannytblack.png", " ", "Bar Crawl", "Start at Buckhead Saloon and make our way down the Buckhead strip", "Nightlife", "Vivek", "Buckhead Bars", " ", false, false, " ", 8, " ", " ", "9:00pm", "1:00am", "March 29, 2019", " ", "Bring a friend. Party all night.");
 
 INSERT INTO events (image, link, title, details, eventType, creator, location, location_address, isPrivate, nicheEvent, niche, score, tentative, attending, start_point, end_point, createDate, price, comments) 
-VALUES (" ", " ", "Braves vs. Reds", "Braves play the Reds in Atlanta", "Sports", "Patrick", "Suntrust Park", " ", false, true, "Braves Games", 9, " ", " ", "7:20pm", "10:00pm", "March 25, 2019", "$38", "Let's go Braves.");
+VALUES ("../client/src/assets/css/plannytblack.png", " ", "Braves vs. Reds", "Braves play the Reds in Atlanta", "Sports", "Patrick", "Suntrust Park", " ", false, true, "Braves Games", 9, " ", " ", "7:20pm", "10:00pm", "March 25, 2019", "$38", "Let's go Braves.");
 
 INSERT INTO events (image, link, title, details, eventType, creator, location, location_address, isPrivate, nicheEvent, niche, score, tentative, attending, start_point, end_point, createDate, price, comments) 
-VALUES (" ", " ", "Top Golf", "Sports", "Friends Night out", "John", "Top Golf Alpharetta", " ", true, false, " ", 10, " ", " ", "7:00am", "9:00pm", "March 16, 2019", "$20", "Bring the competition.");
+VALUES ("../client/src/assets/css/plannytblack.png", " ", "Top Golf", "Sports", "Friends Night out", "John", "Top Golf Alpharetta", " ", true, false, " ", 10, " ", " ", "7:00am", "9:00pm", "March 16, 2019", "$20", "Bring the competition.");
 
 INSERT INTO events (image, link, title, details, eventType, creator, location, location_address, isPrivate, nicheEvent, niche, score, tentative, attending, start_point, end_point, createDate, price, comments) 
-VALUES (" ", " ", "Bootcamp Training", "HIIT Training", "Sports", "Reese", "Peidmont Park", " ", false, true, "Fitness", 8, " ", " ", "11:00am", "12:00pm", "March 16, 2019", " ", "Bring a water bottle and towel. Workout equipment will be provided.");
+VALUES ("../client/src/assets/css/plannytblack.png", " ", "Bootcamp Training", "HIIT Training", "Sports", "Reese", "Peidmont Park", " ", false, true, "Fitness", 8, " ", " ", "11:00am", "12:00pm", "March 16, 2019", " ", "Bring a water bottle and towel. Workout equipment will be provided.");
 
 INSERT INTO events (image, link, title, details, eventType, creator, location, location_address, isPrivate, nicheEvent, niche, score, tentative, attending, start_point, end_point, createDate, price, comments) 
-VALUES (" ", " ", "Bar Crawl 2.0", "Start at Buckhead Saloon and make our way down the Buckhead strip", "Nightlife", "Vivek", "Buckhead Bars", " ", false, false, " ", 8, " ", " ", "9:00pm", "1:00am", "March 30, 2019", " ", "Bring a friend. Party all night.");
+VALUES ("../client/src/assets/css/plannytblack.png", " ", "Bar Crawl 2.0", "Start at Buckhead Saloon and make our way down the Buckhead strip", "Nightlife", "Vivek", "Buckhead Bars", " ", false, false, " ", 8, " ", " ", "9:00pm", "1:00am", "March 30, 2019", " ", "Bring a friend. Party all night.");
 
 INSERT INTO events (image, link, title, details, eventType, creator, location, location_address, isPrivate, nicheEvent, niche, score, tentative, attending, start_point, end_point, createDate, price, comments) 
-VALUES (" ", " ", "Braves vs. Reds", "Braves play the Reds in Atlanta", "Sports", "Patrick", "Suntrust Park", " ", false, true, "Braves Games", 9, " ", " ", "1:10pm", "10:00pm", "March 26, 2019", "$38", "Let's go Braves.");
+VALUES ("../client/src/assets/css/plannytblack.png", " ", "Braves vs. Reds", "Braves play the Reds in Atlanta", "Sports", "Patrick", "Suntrust Park", " ", false, true, "Braves Games", 9, " ", " ", "1:10pm", "10:00pm", "March 26, 2019", "$38", "Let's go Braves.");
 
 INSERT INTO events (image, link, title, details, eventType, creator, location, location_address, isPrivate, nicheEvent, niche, score, tentative, attending, start_point, end_point, createDate, price, comments) 
-VALUES (" ", " ", "Top Golf 2.0", "Friends Night out", "Sports", "John", "Top Golf Alpharetta", " ", true, false, " ", 10, " ", " ", "7:00am", "9:00pm", "March 22, 2019", "$20", "Bring the competition.");
+VALUES ("../client/src/assets/css/plannytblack.png", " ", "Top Golf 2.0", "Friends Night out", "Sports", "John", "Top Golf Alpharetta", " ", true, false, " ", 10, " ", " ", "7:00am", "9:00pm", "March 22, 2019", "$20", "Bring the competition.");
 
 INSERT INTO events (image, link, title, details, eventType, creator, location, location_address, isPrivate, nicheEvent, niche, score, tentative, attending, start_point, end_point, createDate, price, comments) 
-VALUES (" ", " ", "Bootcamp Training", "Core Training", "Sports", "Reese", "Peidmont Park", " ", false, true, "Fitness", 8, " ", " ", "11:00am", "12:00pm", "March 17, 2019", " ", "Bring a water bottle and towel. Workout equipment will be provided.");
+VALUES ("../client/src/assets/css/plannytblack.png", " ", "Bootcamp Training", "Core Training", "Sports", "Reese", "Peidmont Park", " ", false, true, "Fitness", 8, " ", " ", "11:00am", "12:00pm", "March 17, 2019", " ", "Bring a water bottle and towel. Workout equipment will be provided.");
 
 INSERT INTO events (image, link, title, details, eventType, creator, location, location_address, isPrivate, nicheEvent, niche, score, tentative, attending, start_point, end_point, createDate, price, comments) 
-VALUES (" ", " ", "Bowling 2.0", "2 games of bowling with drinks", "Sports", "Reese", "Midtown Bowl", " ", true, true, "Blowing League", 8, " ", " ", "7:00pm", "10:00pm", "March 30, 2019", "$20", "Bring a friend. Food and drinks will be available.");
+VALUES ("../client/src/assets/css/plannytblack.png", " ", "Bowling 2.0", "2 games of bowling with drinks", "Sports", "Reese", "Midtown Bowl", " ", true, true, "Blowing League", 8, " ", " ", "7:00pm", "10:00pm", "March 30, 2019", "$20", "Bring a friend. Food and drinks will be available.");
