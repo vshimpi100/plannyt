286b5cd552257eb51ab573ac824878439256381f
Patrick Shea <patrickshea814@gmail.com>
2019-03-15 18:47:52 -0400

    calendar push

---
+++ client/src/components/profile/Profile.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="Profile">
+      <p> {{inputString}} </p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      auth: true,
+      friends: ['john'],
+      inputString: "Hello World",
+      events: []
+    }
+  }
+}
+</script>
+
+<style>
+</style>
