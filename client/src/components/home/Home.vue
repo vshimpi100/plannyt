a4ac41394f43bcb9ec079da51e1bf9ac0a540014
Vivek Shimpi <vshimpi100@gmail.com>
2019-03-15 18:22:20 -0400

    changes to header and calendar select

--- client/src/components/home/Home.vue
+++ client/src/components/home/Home.vue
@@ -27,20 +27,68 @@
         <div v-else-if="!events[0]" class="col-sm-12 text-center">
           <h3>Awkward, there aren't any events coming up!</h3>
         </div>
+        <div v-else class="col-12 row">
+          <div class="col-md-8">
+            <form>
+              <input
+                class="form-control mr-sm-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              >
+            </form>
+          </div>
+          <div class="col-md-4">
+            <nav aria-label="Page navigation example">
+              <ul class="pagination justify-content-end">
+                <li class="page-item active">
+                  <a
+                    class="page-link"
+                    href="#"
+                    tabindex="-1"
+                    aria-label="Previous"
+                    aria-disabled="true"
+                  >
+                    Week
+                    <i class="fas fa-calendar-week"></i>
+                  </a>
+                </li>
+                <li class="page-item">
+                  <a class="page-link" href="#" aria-label="Next">
+                    Month
+                    <i class="far fa-calendar-alt"></i>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <app-cal-select v-model="curr"></app-cal-select>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import CalSelect from './CalSelect.vue'
+
 export default {
   data () {
     return {
       auth: true,
       friends: ['john'],
-      events: []
+      events: ['event'],
+      curr: new Date(),
     }
-  }
+  },
+  components: {
+    appCalSelect: CalSelect
+  },
+  computed: {
+    formattedDate() {
+      return dateFns.format(this.curr, 'MM/DD/YYYY');
+    }
+  },
 }
 </script>
 
