318314f6fb7da51fb9826dad81169be1d1404cb5
john.mappin89@gmail.com <john.mappin89@gmail.com>
2019-03-14 21:07:44 -0400

    footer

---
+++ client/src/components/shared/Footer.vue
@@ -0,0 +1,72 @@
+<template>
+    <!-- Footer -->
+<footer class="page-footer font-small blue pt-4">
+
+    <!-- Footer Links -->
+    <div class="container-fluid text-center text-md-left">
+
+      <!-- Grid row -->
+      <div class="row">
+
+        <!-- Grid column -->
+        <div class="col-md-6 mt-md-0 mt-3">
+
+          <!-- Content -->
+          <h4 class="text-uppercase">Plannyt</h4>
+          
+
+        </div>
+        <!-- Grid column -->
+
+        <hr class="clearfix w-100 d-md-none pb-3">
+
+        <!-- Grid column -->
+        <div class="col-md-3 mb-md-0 mb-3">
+
+            <!-- Links -->
+            <h5 class="text-uppercase">Contributors</h5>
+
+            <ul class="list-unstyled">
+              <li>
+                <h5 href="#!">Viveck Shimpi</h5>
+              </li>
+              <li>
+                <h5>Patrick Shea</h5>
+              </li>
+              <li>
+                <h5>Reese Fairbanks</h5>
+              </li>
+              <li>
+                <h5>John Mappin</h5>
+              </li>
+            </ul>
+
+
+            
+
+          </div>
+          <!-- Grid column -->
+
+      </div>
+      <!-- Grid row -->
+
+    </div>
+    <!-- Footer Links -->
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3">© 2019 Copyright:
+      <a href="https://github.com/vshimpi100/plannyt"> GITHUB</a>
+    </div>
+    <!-- Copyright -->
+
+  </footer>
+  <!-- Footer -->
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+</style>
